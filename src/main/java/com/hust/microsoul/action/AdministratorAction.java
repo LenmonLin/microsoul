92a1ab0facd48bbfb1f66191c858592e86685f03
qq2057157 <wuster910416@163.com>
2018-01-22 17:01:31 +0800

    测试是是是死是活

--- src/main/java/com/hust/microsoul/action/AdministratorAction.java
+++ src/main/java/com/hust/microsoul/action/AdministratorAction.java
@@ -20,7 +20,7 @@ import com.hust.microsoul.service.GoodsService;
  * @Description:AdministratorAction.java
  * @author  huwentao 
  * @date 创建时间：2018年1月22日 下午2:22:46 
- * @version 1.0   ---1111
+ * @version 1.0   ---11111212312
  */
 @Controller
 @RequestMapping("microsoul/admin")
