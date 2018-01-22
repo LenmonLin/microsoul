8e213f6171f00073b37f151f8bf20d8f9607fb12
qq2057157 <wuster910416@163.com>
2018-01-22 17:03:05 +0800

    啛啛喳喳存储

--- src/main/java/com/hust/microsoul/action/AdvertisementAction.java
+++ src/main/java/com/hust/microsoul/action/AdvertisementAction.java
@@ -3,7 +3,7 @@ package com.hust.microsoul.action;
  * @Description:AdvertisementAction.java
  * @author  huwentao 
  * @date 创建时间：2018年1月22日 下午2:23:17 
- * @version 1.0     ---
+ * @version 1.0     ---qwewqeqwewq
  */
 public class AdvertisementAction {
 
