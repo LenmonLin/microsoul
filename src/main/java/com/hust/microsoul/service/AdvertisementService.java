aea728460e8b5b63aab57752f9cd25add2cd511c
Administrator <Administrator@windows10.microdone.cn>
2018-01-23 09:55:54 +0800

    广告service

--- src/main/java/com/hust/microsoul/service/AdvertisementService.java
+++ src/main/java/com/hust/microsoul/service/AdvertisementService.java
@@ -1,4 +1,11 @@
 package com.hust.microsoul.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.hust.microsoul.model.AdministratorModel;
+import com.hust.microsoul.model.AdvertisementModel;
+
 /** 
  * @Description:AdvertisementService.java
  * @author  huwentao 
@@ -6,5 +13,30 @@ package com.hust.microsoul.service;
  * @version 1.0  
  */
 public interface AdvertisementService {
+	/**
+	 * 增加新广告
+	 * @param request
+	 * @param response
+	 * @param advertisementModel
+	 */
+	public void addAdvertise(HttpServletRequest request,
+			HttpServletResponse response, AdvertisementModel advertisementModel) ;
 
+	/**
+	 * 删除广告
+	 * @param request
+	 * @param response
+	 * @param advertisementModel
+	 */
+    public void delAdvertise(HttpServletRequest request,
+			HttpServletResponse response, AdvertisementModel advertisementModel) ;
+	
+    /**
+	 * 查找广告
+	 * @param request
+	 * @param response
+	 * @param advertisementModel
+	 */
+    public void selectAdvertise(HttpServletRequest request,
+			HttpServletResponse response, AdvertisementModel advertisementModel) ;
 }
--- src/main/java/com/hust/microsoul/service/impl/AdvertisementServiceImpl.java
+++ src/main/java/com/hust/microsoul/service/impl/AdvertisementServiceImpl.java
@@ -1,5 +1,9 @@
 package com.hust.microsoul.service.impl;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.hust.microsoul.model.AdvertisementModel;
 import com.hust.microsoul.service.AdvertisementService;
 
 /** 
@@ -9,5 +13,26 @@ import com.hust.microsoul.service.AdvertisementService;
  * @version 1.0  
  */
 public class AdvertisementServiceImpl implements AdvertisementService {
+
+	@Override
+	public void addAdvertise(HttpServletRequest request, HttpServletResponse response,
+			AdvertisementModel advertisementModel) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delAdvertise(HttpServletRequest request, HttpServletResponse response,
+			AdvertisementModel advertisementModel) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void selectAdvertise(HttpServletRequest request, HttpServletResponse response,
+			AdvertisementModel advertisementModel) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
