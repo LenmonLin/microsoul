6f8fbcaf2989151e4fcb21fad1ca5a4e25a6e007
qq2057157 <wuster910416@163.com>
2018-02-26 15:50:43 +0800

    买家订单状态代码类创建

---
+++ src/main/java/com/hust/microsoul/util/OrderStateCode.java
@@ -0,0 +1,20 @@
+package com.hust.microsoul.util;
+
+/**
+ * @Description:
+ * @author:huwentao
+ * @date:2018年2月26日
+ * @version 1.0  
+ */
+public interface OrderStateCode {
+	public static final Integer UNPAID = 0;
+	
+	public static final Integer UNDELIVERY = 1;
+	
+	public static final Integer UNRECEIVE = 2;
+	
+	public static final Integer RECEIVED = 3;
+	
+	public static final Integer DONE = 9;
+}
+
