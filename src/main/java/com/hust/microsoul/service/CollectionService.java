617672f44d01f4a3032df7f354a54bc2f2fa8a97
ivanslain123 <417447986@qq.com>
2018-03-06 11:20:13 +0800

    新增加collectionservice

---
+++ src/main/java/com/hust/microsoul/service/CollectionService.java
@@ -0,0 +1,21 @@
+package com.hust.microsoul.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Service;
+
+import com.github.pagehelper.PageInfo;
+import com.hust.microsoul.model.CollectionModel;
+import com.hust.microsoul.model.GoodsModel;
+import com.hust.microsoul.model.OrderModel;
+
+@Service
+public interface CollectionService {
+
+	public void addCollection(HttpServletRequest request,HttpServletResponse response, Integer goodsID);
+	
+	public void deleteCollection(HttpServletRequest request,HttpServletResponse response, Integer collectionID);
+	
+	public PageInfo<GoodsModel> collectionList(Integer page, Integer rows,CollectionModel collectionModel);
+}
